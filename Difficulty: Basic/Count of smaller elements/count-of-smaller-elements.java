0a2d60ab37f378e4193a03fae95670ded62106c1
Ujjwal Solanki <75205571+ujjwalsolankii@users.noreply.github.com>
2025-03-28 23:21:43 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Basic/Count of smaller elements/count-of-smaller-elements.java
@@ -0,0 +1,48 @@
+//{ Driver Code Starts
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+
+
+// } Driver Code Ends
+
+
+class Solution {
+    public int countOfElements(int x, List<Integer> arr) {
+        // Code here
+        int count = 0;
+        for(int i=0; i<arr.size(); i++){
+            if(x>=arr.get(i)){
+                count++;
+            }
+        }
+        return count;
+    }
+}
+
+
+//{ Driver Code Starts.
+
+public class GFG {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int t = Integer.parseInt(sc.nextLine());
+        while (t-- > 0) {
+            int x = Integer.parseInt(sc.nextLine());
+            List<Integer> arr = new ArrayList<>();
+            String input = sc.nextLine();
+            StringTokenizer st = new StringTokenizer(input);
+            while (st.hasMoreTokens()) {
+                arr.add(Integer.parseInt(st.nextToken()));
+            }
+            Solution ob = new Solution();
+            int ans = ob.countOfElements(x, arr);
+            System.out.println(ans);
+        
+System.out.println("~");
+}
+        sc.close();
+    }
+}
+
+// } Driver Code Ends
